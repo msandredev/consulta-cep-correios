9aeaad66310beea8c5319e23181f85ff94cf0378
André Meira dos Santos <msandredev@gmail.com>
2023-10-19 17:40:16 -0300

    First commit

---
+++ src/main/java/br/com/msandredev/consultacepcorreios/ConsultaCepCorreiosApplication.java
@@ -0,0 +1,13 @@
+package br.com.msandredev.consultacepcorreios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCepCorreiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCepCorreiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/msandredev/consultacepcorreios/ConsultaCepCorreiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msandredev.consultacepcorreios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCepCorreiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
